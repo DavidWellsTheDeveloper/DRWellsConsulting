936e8959a7714301c0366e20d6ffbe4f3505874e
ricwells <ricwells4002@gmail.com>
2020-01-16 14:54:59 -0700

    changed the base to DRWellsConsulting

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
 
   $request = $_SERVER['REQUEST_URI'];
-
+echo $request;
   switch ($request) {
-        case '/' :
+        case '/DRWellsConsulting/' :
           require __DIR__ . '/home.php';
           break;
         case '' :
           require __DIR__ . '/home.php';
           break;
-        case '/contact' :
+        case '/DRWellsConsulting/contact' :
           require __DIR__ . '/contact.php';
           break;
-        case '/services' :
+        case '/DRWellsConsulting/services' :
           require __DIR__ . '/services.php';
           break;
         default:
-          http_response_code(404);
-          require __DIR__ . '/404.php';
+          //http_response_code(404);
+          //require __DIR__ . '/404.php';
           break;
 }
 
