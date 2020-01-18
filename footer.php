7023b194e2b7d415233b6520f9863b9e25976713
Rick Wells <ricwells4002@hotmail.com>
2020-01-17 20:27:51 -0700

    content and style for footer and services

--- footer.php
+++ footer.php
@@ -1,6 +1,25 @@
 <hr>
-<h2>This is the Footer!</h2>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm" style="text-align: center;">
+			<address>
+			DRWells Consulting (<a href="mailto:ricwells@gmail.com">ricwells@gmail.co</a>)<br/>
+			999-555-1111<br/>
+			1236 Canvasback CT.<br/>
+			Fort Collins, CO 80525
+			</address>
+		</div>
+		<div class="col-sm" style="text-align: center;">
+			Social Media<br/>
+			<span class="social" >
+				<i class="fab fa-facebook-square"></i>
+				<i class="fab fa-twitter-square"></i>
+				<i class="fab fa-instagram"></i>
+			</span>
+		</div>
+	</div>
+</div>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrdave.t.wells@gmail.comap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
--- services.php
+++ services.php
@@ -7,6 +7,14 @@
 
 <!-- Start Page Content -->
 <h1>Services</h1>
+<p>
+We provide a variety of web development services listed below:
+</p>
+<ul>
+	<li>web design</li>
+	<li>web programming</li>
+	<li>logo design</li>
+</ul>
 <!-- End Page Content -->
 
 
