b253bf301d3f299fa4734109acd8fcc85dd5291a
Rick Wells <ricwells4002@hotmail.com>
2020-01-17 19:03:08 -0700

    changed routing and removed .php from nav

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
 
-  $request = $_SERVER['REQUEST_URI'];
+$request = strtoupper(trim($_SERVER['REQUEST_URI'], '/'));
 echo $request;
-  switch ($request) {
-        case '/DRWellsConsulting/' :
-          require __DIR__ . '/home.php';
-          break;
-        case '' :
-          require __DIR__ . '/home.php';
-          break;
-        case '/DRWellsConsulting/contact' :
-          require __DIR__ . '/contact.php';
-          break;
-        case '/DRWellsConsulting/services' :
-          require __DIR__ . '/services.php';
-          break;
-        default:
+switch ($request) {
+  case '' :
+  case 'DRWELLSCONSULTING' :
+  require __DIR__ . '/home.php';
+  break;
+  case 'CONTACT' :
+  case 'DRWELLSCONSULTING/CONTACT' :
+  require __DIR__ . '/contact.php';
+  break;
+  case 'SERVICES' :
+  case 'DRWELLSCONSULTING/SERVICES' :
+  require __DIR__ . '/services.php';
+  break;
+  default:
           //http_response_code(404);
           //require __DIR__ . '/404.php';
-          break;
+  break;
 }
 
 ?>
