1ad4145aa78d798a6c53771f3af3bff3121284f4
DavidWellsTheDeveloper <dave1.t.wells@gmail.com>
2020-01-20 20:36:45 -0700

    fixed navbar added some text to responsive section

--- DRWellsConsulting/resources/views/welcome.blade.php
+++ DRWellsConsulting/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laracast
+                    Laravel
                 </div>
 
                 <div class="links">
--- DRWellsConsulting/routes/web.php
+++ DRWellsConsulting/routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/welcome', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return view('home');
 });
 
 Route::get('/home', function () {
